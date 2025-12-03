3714e4e867efe2ce633ed3fa6cfd5a5d9490818c
Mala-Vigua Many <malmany@f1r11s23.clusters.42paris.fr>
2025-12-03 17:35:46 +0100

    d

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,90 +6,104 @@
 /*   By: malmany <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 12:10:12 by malmany           #+#    #+#             */
-/*   Updated: 2025/12/03 13:58:20 by malmany          ###   ########.fr       */
+/*   Updated: 2025/12/03 17:34:42 by malmany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include<stdio.h>
 
-static int      ft_strln(const char *st)
+int	ft_strln(const char *st)
 {
-        int     res;
+	int	res;
 
-        res = 0;
-        while (st[res])
-                res++;
-        return (res);
+	res = 0;
+	while (st[res])
+		res++;
+	return (res);
 }
 
-size_t  ft_strlcat(char *dst, const char *src, size_t siz)
-{
-        size_t  len_dest;
-        size_t  i;
-        size_t  j;
-        size_t  len_src;
 
-        i = 0;
-        len_dest = ft_strln(dst);
-        len_src = ft_strln(src);
-        j = len_dest;
-        if (siz <= len_dest)
-                return (siz + len_src);
-        while (src[i] && i < (siz - len_dest - 1))
-                dst[j++] = src[i++];
-        dst[j] = '\0';
-        return (len_dest + len_src);
+char	*str_concat_until_nl(char *dst, char *src)
+{
+	size_t	len_dest;
+	size_t	i;
+	size_t	j;
+	size_t	len_src;
+	char	*res;
+
+	len_dest = ft_strln(dst);
+	len_src = ft_strln(src);
+	i = 0;
+	j = 0;
+	res = malloc(len_dest + len_src + 1);
+	if (!res)
+		return (NULL);
+	while (i < len_dest)
+	{	
+		res[i] = dst[i];
+		i++;
+	}
+	while (j < len_src && src[j] != '\n')
+	{
+		res[i] = src[j];
+		i++;
+		j++;
+	}
+	if (src[j] == '\n')
+	{
+		res[i] = '\n';
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
 
 int	is_in_str(char *s, char c)
 {
-        int             i;
-        char    *str;
+	int		i;
+	char	*str;
 
-        str = s;
-        i = 0;
-        while (s[i])
-        {
-                if (s[i] == c)
-                        return (1);
-                i++;
-        }
-        return (0);
+	str = s;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
-
-
 
 char	*get_next_line(int fd)
 {
-	ssize_t r;
-	static ssize_t buf_size;
-	char *buf;
-	char *res;
-	int	end_of_line;
+	static ssize_t	buf_size;
+	ssize_t			r;
+	char			*buf;
+	char			*res;
+	int				end_of_line;
 
+	res = "";
 	r = 1;
 	end_of_line = 0;
-
-	buf_size = 10;
+	buf_size = 5;
 	buf = malloc(buf_size);
 	if (buf)
 	{
-		while(r > 0 && !(end_of_line))
+		while (r > 0 && !(end_of_line))
 		{
-			r = read(fd, buf,buf_size);
+			r = read(fd, buf, buf_size);
 			end_of_line = is_in_str(buf, '\n');
-			if(!(end_of_line))
-				ft_strlcat(res, buf, buf_size);
+			res = str_concat_until_nl(res, buf);
 		}
-		if( r == -1)
+		if (r == -1 || res[0] == '\0')
 			return (NULL);
 		else
 			return (res);
-	
 	}
 	return (NULL);
 }
-#include<stdio.h>
-#include <fcntl.h>
+
+/*#include <fcntl.h>
 int main()
 {
 	char* fileName = "test.txt";
@@ -102,11 +116,11 @@ int main()
     	}
     	else
 	{
-		printf("\nFile %s opened sucessfully!\n", fileName);
 		char *s = get_next_line(fd);
 
 		if (s)
 			printf("%s", s);
+		else
+			printf("NULL");
 	}
-	
-}
+}*/
