20f214589ce341b48081a0ab579a3ef1edb3c006
Mala-Vigua Many <malmany@f4r3s14.clusters.42paris.fr>
2026-01-23 12:36:01 +0100

    d

---
+++ pushSwap/ft_func_for_checker.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_func_for_checker.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malmany <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 12:13:16 by malmany           #+#    #+#             */
+/*   Updated: 2026/01/22 21:01:52 by malmany          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_stack.h"
+#include "get_next_line.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+bool	ft_make_op_to_stack(t_stack_node **a, t_stack_node **b, char *line)
+{
+	if (ft_strcmp(line, "sa\n") == 0)
+		ft_stack_swap(*a, 'a', false);
+	else if (ft_strcmp(line, "sb\n") == 0)
+		ft_stack_swap(*b, 'b', false);
+	else if (ft_strcmp(line, "ss\n") == 0)
+		ft_stack_swap_both(*a, *b, false);
+	else if (ft_strcmp(line, "pa\n") == 0)
+		ft_stack_push(b, a, 'a', false);
+	else if (ft_strcmp(line, "pb\n") == 0)
+		ft_stack_push(a, b, 'b', false);
+	else if (ft_strcmp(line, "ra\n") == 0)
+		ft_stack_rotate(a, 'a', false);
+	else if (ft_strcmp(line, "rb\n") == 0)
+		ft_stack_rotate(b, 'b', false);
+	else if (ft_strcmp(line, "rr\n") == 0)
+		ft_stack_rotate_both(a, b, false);
+	else if (ft_strcmp(line, "rra\n") == 0)
+		ft_stack_reverse_rotate(a, 'a', false);
+	else if (ft_strcmp(line, "rrb\n") == 0)
+		ft_stack_reverse_rotate(b, 'b', false);
+	else if (ft_strcmp(line, "rrr\n") == 0)
+		ft_stack_reverse_rotate_both(a, b, false);
+	else
+		return (false);
+	return (true);
+}
+
+bool	ft_read_and_apply_op(t_stack_node **a, t_stack_node **b)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	while (line)
+	{
+		if (!ft_make_op_to_stack(a, b, line))
+		{
+			free(line);
+			return (false);
+		}
+		free(line);
+		line = get_next_line(0);
+	}
+	return (true);
+}
+
+void	ft_clean_stacks_and_args(t_stack_node **a, t_stack_node **b,
+		char **args, int argc)
+{
+	ft_stack_clear(a);
+	ft_stack_clear(b);
+	ft_clean_args_split(args, argc);
+}
+
+void	ft_verify_stack_is_sorted(t_stack_node *a, t_stack_node *b)
+{
+	if (ft_stack_is_sorted_asc(a) && b == NULL)
+		write(1, "ok\n", 3);
+	else
+		write(1, "ko\n", 3);
+}
--- pushSwap/ft_stack.h
+++ pushSwap/ft_stack.h
@@ -6,7 +6,7 @@
 /*   By: malmany <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 09:34:54 by malmany           #+#    #+#             */
-/*   Updated: 2026/01/22 16:01:20 by malmany          ###   ########.fr       */
+/*   Updated: 2026/01/23 11:49:48 by malmany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_STACK_H
@@ -30,6 +30,7 @@ typedef struct s_stack_node
 	struct s_stack_node	*next;
 }	t_stack_node;
 
+/* stack basic functions in file ft_stack_basic1.c && ft_stack_basic2.c*/
 t_stack_node	*ft_stack_node_new(int value);
 
 void			ft_stack_add_front(t_stack_node **st, t_stack_node *new);
@@ -38,42 +39,18 @@ int				ft_stack_size(t_stack_node *st);
 
 t_stack_node	*ft_stack_last(t_stack_node *st);
 
+void			ft_stack_add_back(t_stack_node **st, t_stack_node *new);
+
 void			ft_stack_delone(t_stack_node *st);
 
 void			ft_stack_clear(t_stack_node **st);
 
-void			ft_stack_print(t_stack_node *st);
+t_stack_node	*ft_stack_new(int *tab, int size);
 
 bool			ft_stack_is_sorted_asc(t_stack_node *st);
 
-t_stack_node	*ft_stack_new(int *tab, int size);
-
-t_stack_node	*ft_stack_get_max(t_stack_node *st);
-
-t_stack_node	*ft_stack_get_min(t_stack_node *st);
-
-void			ft_set_index_and_above_median(t_stack_node *st);
-
-void			ft_set_target_a_from_b(t_stack_node *a, t_stack_node *b);
-
-void			ft_set_target_b_from_a(t_stack_node *a, t_stack_node *b);
-
-void			ft_set_push_cost(t_stack_node *st1, t_stack_node *st2);
-
-void			ft_set_cheapest(t_stack_node *st);
-
-t_stack_node	*ft_get_cheapest(t_stack_node *st);
-
-void			ft_set_a_before_push_to_b(t_stack_node **a, t_stack_node **b);
-
-void			ft_move_cheapest_a_to_b(t_stack_node **a, t_stack_node **b);
-
-void			ft_verify_and_set_top_before_push(t_stack_node **st,
-					t_stack_node *to_be_top, char stack_name);
-
-void			ft_verify_and_set_stacks(t_stack_node **a, t_stack_node **b,
-					t_stack_node *cheapest_node);
-/*stack sorting operation functions*/
+/*stack  basic operation functions 
+ * in ft_stack_basic_op1.c && ft_stack_basic_op2.c*/
 
 void			ft_stack_swap(t_stack_node *st, char stack_name, bool print);
 
@@ -94,35 +71,82 @@ void			ft_stack_reverse_rotate(t_stack_node **st, char stack_name,
 void			ft_stack_reverse_rotate_both(t_stack_node **a, t_stack_node **b,
 					bool print);
 
-/* stack sorting functions*/
+/* stack sorting functions and get max and min of a stack 
+ * in ft_stack_sort_min_max.c*/
+;
+t_stack_node	*ft_stack_get_max(t_stack_node *st);
+
+t_stack_node	*ft_stack_get_min(t_stack_node *st);
 
 void			ft_stack_sort_three(t_stack_node **st);
 
+void			ft_stack_sort_size_greater_three(t_stack_node **a, t_stack_node **b);
+
 void			ft_sort_stack(t_stack_node **a, t_stack_node **b);
 
-/* split */
+/* functions used in ft_sort_stack 
+ * in ft_func_sort_stack1.c && ft_func_sort_stack2.c && ft_func_sort_stack3.c*/
+
+void			ft_set_index_and_above_median(t_stack_node *st);
+
+void			ft_set_target_a_from_b(t_stack_node *a, t_stack_node *b);
+
+void			ft_set_target_b_from_a(t_stack_node *a, t_stack_node *b);
+
+void			ft_set_push_cost(t_stack_node *st1, t_stack_node *st2);
+
+void			ft_set_cheapest(t_stack_node *st);
+
+t_stack_node	*ft_get_cheapest(t_stack_node *st);
+
+void			ft_verify_and_set_top_before_push(t_stack_node **st,
+					t_stack_node *to_be_top, char stack_name);
+
+void			ft_verify_and_set_stacks(t_stack_node **a, t_stack_node **b,
+					t_stack_node *cheapest_node);
+
+void			ft_move_cheapest_a_to_b(t_stack_node **a, t_stack_node **b);
+
+void			ft_set_a_before_push_to_b(t_stack_node **a, t_stack_node **b);
+
+void			ft_set_min_at_top(t_stack_node **a);
+
+/* split in ft_split.c */
 
 char			**ft_split(char const *s, char c);
 
-/* functions for main */
+/* functions used in both main */
+/*files : ft_func_for_mains1.c && ft_func_for_mains2.c*/
 
-bool			is_number(char *c);
+bool			ft_is_number(char *c);
 
 long			ft_atol(char *s);
 
-bool			contains_duplicate(int *int_tab, int size_int_tab);
+bool			ft_contains_duplicate(int *int_tab, int size_int_tab);
 
-bool			is_all_num(int size_args, char **args);
+bool			ft_is_all_num(int size_args, char **args);
 
-int				*create_int_tab(int size_args, char **args);
+int				*ft_create_int_tab(int size_args, char **args);
 
-bool			get_args_and_sizeargs(int argc, char **argv, char ***args,
+bool			ft_get_args_and_sizeargs(int argc, char **argv, char ***args,
 					int *size_args);
 
-bool			parse_and_validate(char **args, int size_args, int **int_tab);
+bool			ft_parse_and_validate(char **args, int size_args,
+					int **int_tab);
 
-void			clean_args_split(char **args, int argc);
+void			ft_clean_args_split(char **args, int argc);
 
-void			init_and_sort_stack(int *int_tab, int size_args);
+/*functions for checker in ft_func_for_checker.c*/
+int				ft_strcmp(const char *s1, const char *s2);
+
+bool			ft_make_op_to_stack(t_stack_node **a, t_stack_node **b,
+					char *line);
+
+bool			ft_read_and_apply_op(t_stack_node **a, t_stack_node **b);
+
+void			ft_clean_stacks_and_args(t_stack_node **a, t_stack_node **b,
+					char **args, int argc);
+
+void			ft_verify_stack_is_sorted(t_stack_node *a, t_stack_node *b);
 
 #endif
