e9f243d5973b548307992aa090cb712d2b087f25
malavigua <105797027+malavigua@users.noreply.github.com>
2026-02-11 00:36:13 +0100

    Add build_col_string function to format collectible data

--- T.c
+++ T.c
@@ -24,3 +24,31 @@ char    *ft_itoa(int n)
     }
     return (str);
 }
+char    *build_col_string(t_mlx_data *d)
+{
+    char    *cur;
+    char    *tot;
+    char    *tmp;
+    char    *msg;
+
+    cur = ft_itoa(d->collected);
+    if (!cur)
+        return (NULL);
+    tot = ft_itoa(d->map->nb_collectible);
+    if (!tot)
+        return (free(cur), NULL);
+    tmp = ft_strjoin("Collectibles: ", cur);
+    if (!tmp)
+        return (free(cur), free(tot), NULL);
+    msg = ft_strjoin(tmp, "/");
+    if (!msg)
+        return (free(cur), free(tot), free(tmp), NULL);
+    free(tmp);
+    tmp = ft_strjoin(msg, tot);
+    if (!tmp)
+        return (free(cur), free(tot), free(msg), NULL);
+    free(cur);
+    free(tot);
+    free(msg);
+    return (tmp);
+}
